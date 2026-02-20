6798480e0c46835dc458cf3338706376102542e8
Lam Vo <lamqvo@gmail.com>
2026-02-20 12:37:53 -0800

    lab4-doubly-linked-list

--- lab4-doubly-linked-list/TextEditor.java
+++ lab4-doubly-linked-list/TextEditor.java
@@ -1,4 +1,59 @@
-package PACKAGE_NAME;
-
 public class TextEditor {
+    private static class Node {
+        String textState;
+        Node prev;
+        Node next;
+        // Node constructor...
+        Node (String textState, Node prev, Node next){
+            this.textState = textState;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+
+    private Node currentNode;
+
+    public TextEditor() {
+        // Start with an initial empty string state.
+        Node initialNode = new Node("", null, null);
+        this.currentNode = initialNode;
+    }
+
+    public void add(String newText) {
+        // Clearing Redo History: When a user types new text, the next pointer of the current node is set to null, effectively deleting the forward (redo) history.
+        currentNode.next = null;
+        // Create a new node with the updated text.
+        String updatedText = currentNode.textState + newText;
+        Node newNode = new Node(updatedText, currentNode, null);
+
+        // Set its 'prev' to the current node.
+        newNode.prev = currentNode;
+        // Set the current node's 'next' to this new node.
+        currentNode.next = newNode;
+        // Finally, update currentNode to point to the new node.
+        currentNode = newNode;
+    }
+
+    public String undo() {
+        // Check if currentNode.prev is not null.
+        if (currentNode.prev != null){
+            // If it is, move currentNode back and return the text.
+            currentNode = currentNode.prev;
+        }
+        // Otherwise, you can't undo.
+        return currentNode.textState;
+    }
+
+    public String redo() {
+        // Check if currentNode.next is not null.
+        if (currentNode.next != null){
+            // If it is, move currentNode forward and return the text.
+            currentNode = currentNode.next;
+        }
+        return currentNode.textState;
+    }
+
+    public void printCurrent() {
+        System.out.println(currentNode.textState);
+    }
 }
--- lab4-doubly-linked-list/TextEditorApp.java
+++ lab4-doubly-linked-list/TextEditorApp.java
@@ -1,4 +1,51 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class TextEditorApp {
+    public static void main(String[] args){
+        TextEditor textEditor = new TextEditor();
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            menu();
+            String option = scanner.nextLine();
+            switch (option) {
+                case "1": {
+                    System.out.println("Enter your text: ");
+                    String newText = scanner.nextLine();
+                    textEditor.add(newText);
+                    break;
+                }
+                case "2": {
+                    System.out.println("Undo: ");
+                    textEditor.undo();
+                    break;
+                }
+                case "3": {
+                    System.out.println("Redo: ");
+                    textEditor.redo();
+                    break;
+                }
+                case "4": {
+                    System.out.println("Print: ");
+                    textEditor.printCurrent();
+                    break;
+                }
+                case "5": {
+                    System.out.println("Exit!");
+                    return;
+                }
+                default: {
+                    System.out.println("Invalid input!");
+                    continue;
+                }
+            }
+        }
+    }
+    private static void menu(){
+        System.out.println("Text Editor Menu.  Pick a number:");
+        System.out.println("1. Enter Text");
+        System.out.println("2. Undo Text");
+        System.out.println("3. Redo Text");
+        System.out.println("4. Print Text");
+        System.out.println("5. Exit");
+    }
 }
